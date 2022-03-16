4d7befb22084f183a5f47cb5f044dc6f5c63d4d3
Evert Arias <evert@iotbits.net>
2022-03-15 23:19:12 -0400

    Increment tabs limit to 20

--- composables/useTabs.js
+++ composables/useTabs.js
@@ -97,7 +97,7 @@ export default function () {
         setCurrentTab(findTab(previous) ?? head(tabs.value));
     };
 
-    const canAddNewTab = computed(() => $config.isDesktop || tabs.value.length < 2);
+    const canAddNewTab = computed(() => $config.isDesktop || tabs.value.length < 20);
 
     const sortedTabs = computed(() =>
         tabs.value.sort((aTab, bTab) => new Date(aTab.created_at) - new Date(bTab.created_at))
